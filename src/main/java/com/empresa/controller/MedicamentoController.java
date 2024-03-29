dfc5b73d2baa03d400b13d5a755bb27ee7d00c07
JORDYROJAS93 <jordy_r12@hotmail.com>
2024-03-29 11:23:54 -0500

    Agregando el metodo POST para tabla medicamento

--- src/main/java/com/empresa/controller/MedicamentoController.java
+++ src/main/java/com/empresa/controller/MedicamentoController.java
@@ -1,14 +1,19 @@
 package com.empresa.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.empresa.entity.Medicamento;
 import com.empresa.service.MedicamentoService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 
@@ -25,6 +30,19 @@ public class MedicamentoController {
 		List<Medicamento> lstsalida = medicamentoService.listaMedicamento();
 		return lstsalida;
 	}
+	
+	@PostMapping
+	public ResponseEntity<?> insertaMedicamento(@RequestBody Medicamento obj){
+		List<String> lstmensajes = new ArrayList<String>();
+		Medicamento objsalida = medicamentoService.insertaMedicamento(obj);
+		if (objsalida == null) {
+			 lstmensajes.add("Error en el registro");
+		 }else {
+			 lstmensajes.add("Se registro la modalidad de id : " + objsalida.getIdMedicamento());
+		 }
+		return ResponseEntity.ok(lstmensajes);
+	}
+	
 	
 
 }
